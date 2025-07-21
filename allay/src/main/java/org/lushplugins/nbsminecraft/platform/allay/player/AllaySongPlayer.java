dfca4a45f606f654bc48f874d92c1ba89c565aa2
OakLoaf <oak@beaconstudios.org>
2025-07-21 10:00:11 +0100

    Changed builder access to static methods instead of constructors

--- allay/src/main/java/org/lushplugins/nbsminecraft/platform/allay/player/AllaySongPlayer.java
+++ allay/src/main/java/org/lushplugins/nbsminecraft/platform/allay/player/AllaySongPlayer.java
@@ -7,8 +7,16 @@ public class AllaySongPlayer {
 
     private AllaySongPlayer() {}
 
+    public static Builder builder() {
+        return new Builder();
+    }
+
     public static class Builder extends SongPlayer.Builder {
 
+        /**
+         * @see #builder()
+         */
+        @Deprecated
         public Builder() {
             super(AllayPlatform.INSTANCE);
         }
--- bukkit/src/main/java/org/lushplugins/nbsminecraft/platform/bukkit/player/BukkitSongPlayer.java
+++ bukkit/src/main/java/org/lushplugins/nbsminecraft/platform/bukkit/player/BukkitSongPlayer.java
@@ -7,8 +7,16 @@ public class BukkitSongPlayer {
 
     private BukkitSongPlayer() {}
 
+    public static Builder builder() {
+        return new Builder();
+    }
+
     public static class Builder extends SongPlayer.Builder {
 
+        /**
+         * @see #builder()
+         */
+        @Deprecated
         public Builder() {
             super(BukkitPlatform.INSTANCE);
         }
