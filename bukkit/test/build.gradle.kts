99c1d27710ee53ff933fb37e6e677580b0ab2459
OakLoaf <oak@beaconstudios.org>
2025-07-31 15:57:43 +0100

    Added test bukkit implementation

---
+++ bukkit/test/build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    id("xyz.jpenilla.run-paper") version("2.2.4")
+}
+
+dependencies {
+    // Dependencies
+    compileOnly("org.spigotmc:spigot-api:1.21.8-R0.1-SNAPSHOT")
+
+    // Projects
+    api(project(":bukkit"))
+
+    // Libraries
+    implementation("io.github.revxrsal:lamp.common:4.0.0-rc.12")
+    implementation("io.github.revxrsal:lamp.bukkit:4.0.0-rc.12")
+}
+
+tasks {
+    processResources{
+        filesMatching("plugin.yml") {
+            expand(project.properties)
+        }
+
+        inputs.property("version", rootProject.version)
+        filesMatching("plugin.yml") {
+            expand("version" to rootProject.version)
+        }
+    }
+
+    runServer {
+        minecraftVersion("1.21.8")
+    }
+}
+
+tasks.withType(xyz.jpenilla.runtask.task.AbstractRun::class) {
+    javaLauncher = javaToolchains.launcherFor {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
